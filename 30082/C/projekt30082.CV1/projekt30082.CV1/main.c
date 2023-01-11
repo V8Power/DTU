e907c00fba0359ea220576ddd07c6d322c6a2444
V8Power <felix@roth-felix.de>
2023-01-12 00:44:51 +0100

    sd

--- 30082/C/projekt30082.CV1/projekt30082.CV1/main.c
+++ 30082/C/projekt30082.CV1/projekt30082.CV1/main.c
@@ -25,14 +25,14 @@
 #include "uart.h"
 
 
-uint8_t a = 0;
+uint16_t a = 0;
 uint32_t micro = 0;
 
 char test_data[20] = {0x55, 0xAA,0x00,0x14,0x02,0x20,0x23,0x56,0x78, 0x98, 0xA5, 0xE5, 0xFF, 0xC3, 0xA4, 0x99, 0x78, 0x50, 0x00, 0x00};
 //timers
 uint16_t cycle_time = 1;
 uint32_t prev_cycle_time = 0;
-
+char SPI_read_data[10];
 uint32_t micros(){
 	return micro;
 }
@@ -68,44 +68,42 @@ int main (void){
 	uart_init(115200);
 	
 	while (1){
-		if (micros() - prev_cycle_time >= 4000){    
+		if (micros() - prev_cycle_time >= 1000){    
 				prev_cycle_time = micros();
-				/*if (a == 20){
-				SPI_write(0x5);
-				}
 				
-				else if (a == 40){
-				SPI_write(0xFE);
-				}
-				else if (a == 60){
-				SPI_write((0x03));
-				}
-				else if (a == 80){
-				SPI_write(0xFF);
-				}
-				else if (a == 100){
-				uint8_t checksum = 0x05 ^ 0xFE ^ 0x03 ^ 0xFF;
-				SPI_write(checksum);
-				}*/
-				if (PINB & (1 << PB6)) {
-					SPI_write(0xAA);
-				}
-				//uart_send_char(SPI_read_write(0xC3));
-				//SPI_write(0xAA);
-				//SPI_write(a);
-				PORTB &= ~(1 << PB0);
+				/*PORTB &= ~(1 << PB0);
 				//char spi_data = SPI_read();
 				
-				char spi_data = SPI_read_write(0xAF);
+				char spi_data = SPI_read_write(0xAB);
 				PORTB |= (1 << PB0);
 				uart_send_char(spi_data);
 				a++;
 				/*if (a== 255){
 					a=0;
 				}*/
-				for (int v = 0; v < 20; v++){
-				//uart_send_char(test_data[v]);
+				if (a < 5) {
+					SPI_read();
+					
 				}
+				else if (a == 5) {
+					PORTB &= ~(1 << PB0);
+					
+					
+					SPI_read_data[a -5] = SPI_read_write(0xAA);
+					PORTB |= (1 << PB0);
+				}
+				else {
+					SPI_read_data[a-5] = SPI_read();
+				}
+				a++;
+				
+				if (a > 12) {
+					a = 0;
+					for (int v = 0; v < 10; v++){
+					uart_send_char(SPI_read_data[v]);	
+					}
+				}
+				
 			}
 			
 			
--- 30082/VHDL/Signal_gen_2022/SigGenControl.vhd
+++ 30082/VHDL/Signal_gen_2022/SigGenControl.vhd
@@ -77,7 +77,8 @@ signal tx  : std_logic;
 signal send_data : std_logic_vector(7 downto 0 );
 signal send_count : natural;
 signal sent_packets : std_logic_vector(7 downto 0 );
-signal send_state : std_logic;
+
+signal SS2: std_logic;
 
 begin
 
@@ -184,50 +185,63 @@ begin
 
   end case;
 end process;
-
-
-
-PROCESS (SCK, MOSI, SS, SCK2)
+PROCESS (SCK, MOSI, SS, SCK2, send, SS2, SHIFTREG_data, send_data)
 
 BEGIN
-
-
---IF (SS = '0') THEN
-    
-    if (SCK'event and SCK = '1') then
-       -- if (send = '1') then
-       -- SHIFTREG <= send_data;
-        --send <= '0';
-      --  else
+--send_data <= x"A1";
+if (SCK'event and SCK = '1') then
+        
         IF (SS = '0') THEN
         SHIFTREG <= SHIFTREG(6 downto 0) & MOSI;
         --END IF;
         
-            send_count <= send_count -1;
+            
         END IF;
     END IF;
     if falling_edge(SCK) then
      --send_data <= SHIFTREG;
+        
+        
         IF (SS = '0') THEN    
-        MISO <=   send_data(send_count);
-        Stat2 <= '1';
-        send_state <= '1';    
+        
+        
+            send_count <= send_count -1;
         END IF;
     END IF;  
- --SHIFTREG_out <= SHIFTREG(7 DOWNTO 0);
- --MISO <=  not send_data(send_count);
 
-if SS = '1' then
-MISO <= '1';
-send_count <= 7;
+if SS = '0' then
+    MISO <=   send_data(send_count);
+    Stat1 <= '1';
+END IF;
+if SS = '1' then 
+    MISO <= '1';
+    send_count <= 7;
+    SHIFTREG_data <= SHIFTREG; 
+    --SHIFTREG_out <= Shiftreg;
+    Stat1 <= '0';
+END IF;
+end process;
+
+
+PROCESS (SCK, MOSI, SS, SCK2, send, SS2, sent_packets, send, Clk,  SHIFTREG_data, send_data)
+
+BEGIN
+
+
+if (SS'event and SS = '1') then
+
+
 Stat5 <= '0';
+Stat2 <= '0';
+
 --END IF;
-    if (send = '1' and send_count = 7 and send_state = '1') then
-            sent_packets <= sent_packets + 1;
-            send_state <= '0';
-            END IF;
+     if (send = '1' ) then
+            sent_packets <= sent_packets + "1";
+     else 
+     --sent_packets <= x"00";       
+     END IF;  
   --if rising_edge (SS) then
-   SHIFTREG_out <= SHIFTREG(7 DOWNTO 0);
+   --SHIFTREG_out <= SHIFTREG(7 DOWNTO 0);
   --SHIFTREG <= x"39";
   --send_data <= SHIFTREG_data;
     if ID_ok = '1' then
@@ -235,19 +249,21 @@ Stat5 <= '0';
     else 
     Pack_count <= "000000";
     END IF;
-    SHIFTREG_data <= SHIFTREG; 
     
-  END IF;
+
+  
+ --if Clk'event and Clk = '1' then
+ 
   if SHIFTREG_data = x"05" then
     ID_ok <= '1';
     ID <= ShIFTREG_data;
-    Stat1 <= '1';
+    --Stat1 <= '1';
     --Stat2 <= '0';
-    Stat3 <= '0';
+    --Stat3 <= '0';
     Stat4 <= '0';
     --Stat5 <= '0';
     
-  elsif SHIFTREG_data = x"AA" and send = '0' then
+  elsif (SHIFTREG_data = x"AA" and send = '0') then
     Stat4 <= '1';
     --send_data <= x"39";
     send <= '1';
@@ -257,7 +273,7 @@ Stat5 <= '0';
     --Stat2 <= '1';
   elsif PACK_count = x"2" then
     FREQ_SPI <= SHIFTREG_data;
-    Stat3 <= '1';
+    --Stat3 <= '1';
   elsif PACK_count = x"3" then
     Shape_stat_SPI <= SHIFTREG_data;
     --Stat4 <= '1';
@@ -268,29 +284,38 @@ Stat5 <= '0';
     
         --Stat5 <= '1';
     END IF;
-    Stat1 <= '0';
+    --Stat1 <= '0';
     
   END IF;
 if send = '1' then
-    if sent_packets = x"0" then
+    if sent_packets = x"00" then
         send_data <= x"AA";
         
-    elsif sent_packets = x"1" then
+    elsif sent_packets = x"01" then
         send_data <= x"FF";
-    elsif sent_packets = x"2" then
+    elsif sent_packets = x"02" then
         send_data <= Freq;
-    elsif sent_packets = x"3" then
+    elsif sent_packets = x"03" then
         send_data <= Ampl;
-    elsif sent_packets = x"4" then
+    elsif sent_packets = x"04" then
         send_data <= Shape;
-    elsif sent_packets = x"5" then 
+    elsif sent_packets = x"05" then 
         send_data <= (x"AA" xor x"FF" xor Freq xor Ampl xor Shape);
         send <= '0';
-     
+        sent_packets <= x"00";
+    else
+        --sent_packets <= x"00"; 
+        
        
     END IF;
+--sent_packets <= x"00"; 
+END IF;
 END IF;
   --SHIFTREG_out (5 downto 0)<= Pack_count;
+  SHIFTREG_out <= sent_packets;
+  stat3 <= send;
+ 
+SS2 <= SS;
    END PROCESS;
        
 
